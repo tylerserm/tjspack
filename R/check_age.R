c893f460e0dcb3c76a60a642167c9c0b1731341c
Tyler Serm <tylerserm@gmail.com>
2024-12-19 19:43:18 -0500

    check_visit_num and check_visit_order commit

--- R/check_age.R
+++ R/check_age.R
@@ -1,9 +1,9 @@
-#' @title Check Age in longitudinal data
+#' @title Check and Correct Age in Longitudinal Data
 #'
-#' @description This function checks to ensure a subject's age is consistant over longitudinal data.
+#' @description This function checks to ensure a subject's age is consistent over longitudinal data. It will also correct any incorrect ages.
 #'
-#' @param Subj_ID the patients identifying number
 #' @param df the dataframe you would like applied to this function
+#' @param Subj_ID the patients identifying number
 #' @param testdate the date the sample was collected
 #' @param DOB the patients date of birth
 #' @param Age the patients age
@@ -25,34 +25,24 @@
 Example_Dataset<-read.csv("C:/Users/tyler/Downloads/examplespreadsheet.csv")
 
 check_age <- function(df, Subj_ID, testdate, DOB, Age) {
-
-  # Ensure the columns are in Date format
   df[[testdate]] <- as.Date(df[[testdate]], format = "%m/%d/%Y")
   df[[DOB]] <- as.Date(df[[DOB]], format = "%m/%d/%Y")
 
-  # Calculate the expected age based on the difference between the dates
   df$calculated_age <- round(as.numeric(difftime(df[[testdate]], df[[DOB]], units = "weeks")) / 52.25, 2)
 
-  # Check if the calculated age matches the provided age
   df$age_check <- abs(df$calculated_age - df[[Age]]) < 0.01
 
-  # Iterate through rows where age_check is FALSE (incorrect age)
   for (i in which(df$age_check == FALSE)) {
-    # Find other rows with the same ID and correct age
     correct_rows <- df[df[[Subj_ID]] == df[[Subj_ID]][i] & df$age_check == TRUE, ]
 
     if (nrow(correct_rows) > 0) {
-      # Replace the incorrect Birthday with the correct one (from the first row found)
       df[[DOB]][i] <- correct_rows[[DOB]][1]
 
-      # Recalculate the age based on the corrected Birthday
       df$calculated_age[i] <- as.numeric(difftime(df[[testdate]][i], df[[DOB]][i], units = "weeks")) / 52.25
       df[[Age]][i] <- round(df$calculated_age[i], 2)
-      df$age_check[i] <- TRUE  # Mark this row as having a correct age now
+      df$age_check[i] <- TRUE
     }
   }
   df <- df[, !names(df) %in% c("age_check","calculated_age")]
   return(df)
 }
-
-df <- check_age(Example_Dataset, "Lead.Specimen.RecordedDate", "Birthday", "Age","ID")
--- R/check_visit_order.R
+++ R/check_visit_order.R
@@ -1,47 +1,54 @@
-#' @title My Hello World Function
+#' @title Check and Correct Visit Order Number in Longitudinal Data
 #'
-#' @description
+#' @description This function checks to ensure a subject's visit order number is correct over longitudinal data. It will also correct any incorrect entries.
 #'
-#' @param df
+#' @param df the dataframe you would like applied to this function
+#' @param Subj_ID the patients identifying number
+#' @param testdate the date the sample was collected
+#' @param visit_num the visit number, the record of which instance the patient was seen
 #'
-#' @returns
+#' @returns The entered dataframe now with any incorrectly entered visit order number information fixed
 #' @export
 #'
 #' @examples
-check_and_fix_visit_num_order <- function(df) {
-  # Sort the data by ID and Lead.Specimen.RecordedDate for proper date comparison
-  df <- df[order(df$ID, df$Lead.Specimen.RecordedDate), ]
+#' This example dataset has an incorrectly entered in the visit_num column. The second and third
+#' #visit_num for ID 1 are accidentally reversed.
+#' example <- data.frame(
+#' ID = c(1, 1, 1, 2, 2),
+#' TestDate = as.Date(c("2023-01-01", "2023-02-01", "2023-04-01", "2023-02-01", "2023-02-01")),
+#' visit_num = c(1, 3, 2, 1, 2)
+#' check_Age(example, "ID", "TestDate", "DOB", "Age")
+#' #Notice now, the incorrect visit numbers have been corrected
+#' example
+check_visit_order <- function(df, Subj_ID, testdate, visit_num) {
+  df <- df[order(df[[Subj_ID]], df[[testdate]]), ]
 
-  for (subject_id in unique(df$ID)) {
-    subject_data <- df[df$ID == subject_id, ]
+  for (subject_id in unique(df[[Subj_ID]])) {
+    subject_data <- df[df[[Subj_ID]] == subject_id, ]
 
     for (i in 1:nrow(subject_data)) {
-      if (subject_data$Visit.Num[i] %in% subject_data$Visit.Num[c(1:(i-1), (i+1):nrow(subject_data))]) {
+      if (subject_data[[visit_num]][i] %in% subject_data[[visit_num]][c(1:(i-1), (i+1):nrow(subject_data))]) {
 
-        if (!subject_data$Lead.Specimen.RecordedDate[i] %in% subject_data$Lead.Specimen.RecordedDate[c(1:(i-1), (i+1):nrow(subject_data))]) {
-          df$Visit.Num[which(df$ID == subject_id & df$Lead.Specimen.RecordedDate == subject_data$Lead.Specimen.RecordedDate[i] )] <- subject_data$Visit.Num[i-1] + 1
+        if (!subject_data[[testdate]][i] %in% subject_data[[testdate]][c(1:(i-1), (i+1):nrow(subject_data))]) {
+          df[[visit_num]][which(df[[Subj_ID]] == subject_id & df[[testdate]] == subject_data[[testdate]][i] )] <- subject_data[[visit_num]][i-1] + 1
         }
       }
     }
     for (n in 1:(nrow(subject_data)-1)) {
-      if (nrow(subject_data)>=2 && subject_data$Visit.Num[n]>subject_data$Visit.Num[n+1]){
-        subject_data$Visit.Num[n] = subject_data$Visit.Num[n+1] - 1
+      if (nrow(subject_data)>=2 && subject_data[[visit_num]][n]>subject_data[[visit_num]][n+1]){
+        subject_data[[visit_num]][n] = subject_data[[visit_num]][n+1] - 1
       }
     }
-    full_sequence <- seq(min(subject_data$Visit.Num), max(subject_data$Visit.Num))
+    full_sequence <- seq(min(subject_data[[visit_num]]), max(subject_data[[visit_num]]))
 
-    # Check for missing numbers in the Visit.Num sequence
-    missing_nums <- setdiff(full_sequence, subject_data$Visit.Num)
+    missing_nums <- setdiff(full_sequence, subject_data[[visit_num]])
 
     if (length(missing_nums) > 0) {
-      # If there are missing numbers, start adjusting Visit.Num from the first missing number
       for (missing_num in missing_nums) {
-        # Find the first row after the missing number
-        first_after_missing <- which(subject_data$Visit.Num > missing_num)[1]
+        first_after_missing <- which(subject_data[[visit_num]] > missing_num)[1]
 
-        # Adjust Visit.Num starting from that row onward
-        for (x in which(subject_data$Visit.Num == first_after_missing):nrow(subject_data) ){
-          df$Visit.Num[which(rownames(df) ==rownames(subject_data)[x])] <- subject_data$Visit.Num[x]-1
+        for (x in which(subject_data[[visit_num]] == first_after_missing):nrow(subject_data) ){
+          df[[visit_num]][which(rownames(df) ==rownames(subject_data)[x])] <- subject_data[[visit_num]][x]-1
         }
       }
     }
@@ -50,5 +57,3 @@ check_and_fix_visit_num_order <- function(df) {
 
   return(df)
 }
-
-df <- check_and_fix_visit_num_order(df)
